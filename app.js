72635d22da2e314815ac73d1789ea7af125d07ca
annearbor <43568777+annearbor@users.noreply.github.com>
2019-01-19 17:53:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -53,6 +53,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+const auth = require('./routes/auth');
+app.use('/', auth);
 
 
 module.exports = app;
