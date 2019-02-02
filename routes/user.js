a77973e59b99c0076c84b213e5c81f9f2cd6f18e
annearbor <betting.anne@gmail.com>
2019-02-02 11:40:16 +0100

    adds authentication method for private routes

--- routes/user.js
+++ routes/user.js
@@ -2,36 +2,26 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 
-// const ensureLogin = require("connect-ensure-login");
+function ensureLogin(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/login");
+  }
+}
 
-// authRoutes.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-//   res.render("private", { user: req.user });
-// });
-
-// function ensureLogin(req, res, next) {
-//   if (req.isAuthenticated()) {
-//     return next();
-//   } else {
-//     res.redirect("/login");
-
-// function ensureAuthenticated(req, res, next) {
-//   if (req.isAuthenticated()) {
-//     return next();
-//   } else {
-//     res.redirect("/login");
-
-router.get("/profile/show", (req, res, next) => {
+router.get("/profile/show", ensureLogin, (req, res, next) => {
   res.render("user/show", { user: req.user }); // shows current user information, can then be edited
   console.log(req.user);
 });
 
-router.get("/profile/edit", (req, res, next) => {
+router.get("/profile/edit", ensureLogin, (req, res, next) => {
   console.log(req.user);
   console.log("this is a GET call");
   res.render("user/edit", { user: req.user }); // show the edit page, data can be edited here
 });
 
-router.post("/profile/edit", (req, res, next) => {
+router.post("/profile/edit", ensureLogin, (req, res, next) => {
   const { firstName, lastName, role, currentPosition, summary } = req.body;
   console.log(req.body);
   console.log("did this route get called");
