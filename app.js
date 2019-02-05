1183e5b3a29cab7a2841a42f926019bf469e9270
annearbor <betting.anne@gmail.com>
2019-02-05 20:16:40 +0100

    corrects merge failures

--- app.js
+++ app.js
@@ -68,11 +68,7 @@ const auth = require("./routes/auth");
 app.use("/", auth);
 const user = require("./routes/user");
 app.use("/", user);
-<<<<<<< HEAD
-//const users = require....
-=======
 const search = require("./routes/search");
 app.use("/", search);
->>>>>>> 69a42dadde231eaf9534cb67e04ea367456eb94f
 
 module.exports = app;
